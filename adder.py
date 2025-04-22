f24dd820ed28f18ca053cf9f922bc5375662199a
coevol <rpajhc@gmail.coml>
2025-04-22 15:50:31 +0900

    fix Addfunction main

--- adder.py
+++ adder.py
@@ -1,9 +1,14 @@
 # adding two numbes and returm the sum
 
-def adder(num1, num2):
+def adder(num1: int, num2: int):
     sum = num1 + num2
     return sum
 
 def info():
     msg = "this function adds two numbers"
     print(msg)
+
+if __name__=='__main__':
+    print(adder(3, 7))
+
+
